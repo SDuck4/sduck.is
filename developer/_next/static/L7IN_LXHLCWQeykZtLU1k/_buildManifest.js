eedeb613497df7c2f25756ae49c5f4d47468afbe
SDuck <iam@sduck.is>
2021-03-22 20:19:41 +0900

    Update developer

--- developer/_next/static/L7IN_LXHLCWQeykZtLU1k/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002F0f1ac474.3febc725da045b754233.js","static\u002Fchunks\u002Fpages\u002Findex-cadb8bb7a1a8926e69c4.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-f7a70b7323f6c29567ab.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
